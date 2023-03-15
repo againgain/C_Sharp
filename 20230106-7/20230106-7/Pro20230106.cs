bfcb02f4777656f24b0df61096342de2d6551347
againgain <127922866+againgain@users.noreply.github.com>
2023-03-16 00:15:35 +0900

    Add files via upload

---
+++ 20230106-7/20230106-7/Pro20230106.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class oooo
+{
+    public static void Main()
+    {
+        int tmp = 0;
+        Console.WriteLine("最大公約数を求めます。\n数を２つ入力してください。");
+        Console.Write("1つ目> ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("２つ目> ");
+        int n = int.Parse(Console.ReadLine());
+
+        
+
+
+        while (n>0)
+        {
+            Console.WriteLine("{0}, {1}",m,n);
+            if (n > m)
+            {
+                (m, n) = (n, m);
+            }
+
+
+            /*
+            if (n == 0)
+            {
+                
+                break;
+            }
+            */
+
+            tmp = m % n;
+            m = n;
+            n = tmp;
+
+            //Console.WriteLine("2：{0}, {1}", m, n);
+        }
+        Console.WriteLine("最大公約数は {0} です。", m);
+
+    }
+}
---
+++ 20230106-8/20230106-8/Pro20230106-8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+class aaa
+{
+    public static void Main()
+    {
+        int[] a = new int[5];
+
+        a[0] = 26;
+        a[1] = 170;
+        a[2] = a[0] + a[1];
+
+        int n = 3;
+        a[n] = n;
+        n++;
+        a[n] = a[0] * 3;
+
+        for(int i=0; i<5; i++)
+        {
+            Console.WriteLine("a[{0}] = {1}", i, a[i]);
+        }
+    }
+
+
+}
+*/
+
+class Array06
+{
+    public static void Main()
+    {
+        int[] a = { 26, 170, 309, 310, 371 };
+        int[] b = { 1, 2, 3, 4, 5 };
+        int[] c = new int[5];
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            c[i] = a[i] * b[i];
+            Console.WriteLine("c[{0}] = {1}", i, c[i]);
+        }
+    }
+}
