d2faf87d0b0287b9e2ed6e231c2b842ea3bc77fe
againgain <127922866+againgain@users.noreply.github.com>
2023-03-16 00:29:55 +0900

    Add files via upload

---
+++ Ronri2/Ronri2/Ronri2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Ronri
+{
+    public static void Main()
+    {
+        double a, b, c, p, q, r;
+
+        Console.WriteLine("パラメータを入力してください。");
+        Console.WriteLine(" a = ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine(" b = ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine(" c = ");
+        c = double.Parse(Console.ReadLine());
+
+        p = 3 * a - b;
+        q = a * a + 3 * c;
+        r = 2 * (b - c) / a;
+
+        Console.WriteLine("p = {0}, q = {1}, r = {2}", p, q, r);
+    }
+}
